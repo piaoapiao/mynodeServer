a338d4753ba88d5070b2433e60844a58d75e2935
guodong <guodong@guodongdeMacBook-Pro.local>
2017-04-07 08:50:32 +0800

    add login url

--- api/user/index.js
+++ api/user/index.js
@@ -4,8 +4,8 @@ var express = require('express');
 
 var router = express.Router();
 
-var controller = require('./userController');
+var controller = require('./userController');   
 
-router.get('/login', controller.login);
+router.get('/login', controller.login);  // http://localhost:3000/user/login
 
 module.exports = router;

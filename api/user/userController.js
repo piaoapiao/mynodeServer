aeeec7ec876bbeed53263111076a60745d822c8d
guodong <guodong@guodongdeMacBook-Pro.local>
2017-04-11 20:26:47 +0800

    send 2 request

--- api/user/userController.js
+++ api/user/userController.js
@@ -1,12 +1,50 @@
 'use strict';
+// function login(req, res, next) {
+//    res.type('text/plain');
+//   // res.staus(200);
+//   res.send("login ok");
+//
+// }
+
+var  axios = require('axios');
+
 function login(req, res, next) {
-  res.type('text/plain');
+  // res.type('text/plain');
   // res.staus(200);
-  res.send("login ok");
-  
+  // res.send("login ok");
+    requestbaidu(res);
 }
 
-// module.exports.login = login;
+//  http://www.baidu.com
+//  http://www.nonobank.com
+
+function  requestbaidu(res) {
+    var instance = axios.create();
+
+    var rtdata = instance.get('http://www.hnie.edu.cn', {
+        timeout: 5000
+    }).then(function (response) {
+        console.log("-----");
+        console.log(response.data);
+        requestbaidu2(response.data,res);
+        //res.type('text/plain');
+        //res.send(response.data);
+    });
+}
+
+function  requestbaidu2(result,res) {
+    var instance = axios.create();
+
+    var rtdata = instance.get('http://www.baidu.com', {
+        timeout: 5000
+    }).then(function (response) {
+        console.log(response.data);
+        result = result + response;
+        // result =  response.data;
+        res.send(result);
+    });
+}
+
 
 module.exports = {
   login:login

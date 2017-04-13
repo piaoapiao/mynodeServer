5e449351578df1a044a97abc4d8d5915d29a273b
guodong <guodong@guodongdeMacBook-Pro.local>
2017-04-13 12:41:35 +0800

    axios spread two request

--- api/user/userController.js
+++ api/user/userController.js
@@ -8,41 +8,86 @@
 
 var  axios = require('axios');
 
+// function login(req, res, next) {
+//   // res.type('text/plain');
+//   // res.staus(200);
+//   // res.send("login ok");
+//     requestbaidu(res);
+// }
+//
+// //  http://www.baidu.com
+// //  http://www.nonobank.com
+//
+// var instance = axios.create();
+//
+// function  requestbaidu(res) {
+//     //var instance = axios.create();
+//
+//     var rtdata = instance.get('http://www.hnie.edu.cn', {
+//         timeout: 5000
+//     }).then(function (response) {
+//         console.log("-----");
+//         console.log(response.data);
+//         requestbaidu2(response.data,res);
+//         //res.type('text/plain');
+//         //res.send(response.data);
+//     });
+// }
+//
+// function  requestbaidu2(result,res) {
+//     //var instance = axios.create();
+//
+//     var rtdata = instance.get('http://www.baidu.com', {
+//         timeout: 5000
+//     }).then(function (response) {
+//         console.log(response.data);
+//         //result = result + response;
+//          result =  response.data;
+//         res.send(result);
+//     });
+// }
+
+
+function getNono(){
+    return axios.get('http://www.nonobank.com');
+}
+
+function gethnie(){
+    return axios.get("http://www.hnie.edu.cn");
+}
+
+
 function login(req, res, next) {
-  // res.type('text/plain');
-  // res.staus(200);
-  // res.send("login ok");
-    requestbaidu(res);
-}
+    // res.type('text/plain');
+    // res.staus(200);
+    // res.send("login ok");
+    //requestbaidu(res);
 
-//  http://www.baidu.com
-//  http://www.nonobank.com
+    axios.all([getNono(),gethnie()])
+        .then(axios.spread( function(baidu,hnie){
+        console.log(baidu);
+            // console.log(typeof baidu);
+            //  console.log(baidu.data);
+            // console.log(typeof baidu);
+            console.log("here");
+            res.type('text/plain');
+            // res.status(200);
+            //res.send(baidu.data);
+           // res.send(hnie.data);
 
-function  requestbaidu(res) {
-    var instance = axios.create();
+            //res.send(hnie.data) + baidu.data);
 
-    var rtdata = instance.get('http://www.hnie.edu.cn', {
-        timeout: 5000
-    }).then(function (response) {
-        console.log("-----");
-        console.log(response.data);
-        requestbaidu2(response.data,res);
-        //res.type('text/plain');
-        //res.send(response.data);
-    });
-}
+            res.send(hnie.data+ baidu.data);
 
-function  requestbaidu2(result,res) {
-    var instance = axios.create();
+    }))
+        .catch(function (e) {
+            console.log(e);
+            console.log("Promise Rejected");
+        });
 
-    var rtdata = instance.get('http://www.baidu.com', {
-        timeout: 5000
-    }).then(function (response) {
-        console.log(response.data);
-        result = result + response;
-        // result =  response.data;
-        res.send(result);
-    });
+    // res.type('text/plain');
+    // res.staus(200);
+    // res.send("login ok");
 }
 
 

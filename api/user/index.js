369d5cb11b1ec3f3e65f12fdb84a3a18eb2eb900
guodong <guodong@guodongdeMacBook-Pro.local>
2017-04-14 22:57:30 +0800

    send post form ok

--- api/user/index.js
+++ api/user/index.js
@@ -4,7 +4,9 @@ var express = require('express');
 
 var router = express.Router();
 
-var controller = require('./userController');   
+var controller = require('./userController');
+
+//http://localhost:3000/user/login
 
 router.get('/login', controller.login);  // http://localhost:3000/user/login    http:/192.168.34.109:3000/user/login
 
--- api/user/userController.js
+++ api/user/userController.js
@@ -8,25 +8,72 @@
 
 var  axios = require("axios");
 var urlencode = require("urlencode");
+var querystring = require('querystring');
+
+
+// var instance = axios.create({data: {
+//     request: '{"sendType":"3","mobileNumber":"13262591961"}'
+// }});
+
+
+// var instance = axios({
+//     method: 'post',
+//     url: 'http://www.theunifi.com/nono-web/uniFi/sendSmsCode',
+//     data: {
+//         request: '{"sendType":"3","mobileNumber":"13262591961"}'
+//     }
+// });
 
 var instance = axios.create();
 
+instance.defaults.headers.post['content-type'] = 'application/x-www-form-urlencoded';
+
 function login(req, res, next) {
-  // res.type('text/plain');
-  // res.staus(200);
-  // res.send("login ok");
-    //requestbaidu(res);
-    getHttpsGithub(res);
+
+    console.log("login");
+    //res.send("hello");
+    postUnifi(res);
 }
 
 function getHttpsGithub(res)
 {
     axios.get('https://api.github.com/users/' + "piaoapiao")
+    //axios.get("https://www.nonobank.com")
         .then(function(response){
             console.log(response.data);
             console.log(response.status);
             res.send(response.data);
-        });
+        })
+        .catch(function (e) {
+            console.log(e);
+            res.send(e);
+        })
+        ;
+}
+
+//'request':'{"sendType":"3","mobileNumber":"13262591961"}'
+
+function postUnifi(res)
+{
+    // let param = new FormData();
+   // param.append("request",'{"sendType":"3","mobileNumber":"13262591961"}');
+
+    axios.post('http://www.theunifi.com/nono-web/uniFi/sendSmsCode',
+        {'request':'{"sendType":"3","mobileNumber":"13262591961"}'}
+        ,{transformRequest: function (data) {
+    // Do whatever you want to transform the data
+         return querystring.stringify(data)
+    }})
+        .then(function(response){
+            console.log(response.data);
+            console.log(response.status);
+            res.send(response.data);
+        })
+        .catch(function (e) {
+            console.log(e);
+            res.send(e.message);
+        })
+    ;
 
 }
 
